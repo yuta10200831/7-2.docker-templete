7f244a3d04c7e333c082dfcde458900e4071db14
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-07 16:21:29 +0900

    fix:ガード節を使用する

--- src/public/user/signin_complete.php
+++ src/public/user/signin_complete.php
@@ -6,22 +6,24 @@ $pdo = new PDO('mysql:host=mysql; dbname=blog; charset=utf8', 'root', 'password'
 $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
 
-$stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
-$stmt->execute([$email]);
-$user = $stmt->fetch();
-
 if (empty($email) || empty($password)) {
     $_SESSION['error'] = "パスワードとメールアドレスを入力してください";
     header('Location: signin.php');
     exit;
-} elseif ($user && password_verify($password, $user['password'])) {
-    $_SESSION['username'] = $user['name']; 
-    $_SESSION['user_id'] = $user['id'];
-    header('Location: /index.php');
-    exit;
-} else {
+}
+
+$stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+$stmt->execute([$email]);
+$user = $stmt->fetch();
+
+if (!$user || !password_verify($password, $user['password'])) {
     $_SESSION['error'] = "メールアドレスまたはパスワードが違います";
     header('Location: signin.php');
     exit;
 }
+
+$_SESSION['username'] = $user['name'];
+$_SESSION['user_id'] = $user['id'];
+header('Location: /index.php');
+exit;
 ?>
