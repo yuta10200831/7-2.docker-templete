0c41af2ad0990668ff06ee24760838fb3503329b
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-30 22:15:34 +0900

    fix:create.phpをブラッシング

--- src/public/create.php
+++ src/public/create.php
@@ -8,7 +8,8 @@ if (!isset($_SESSION['user']['name'])) {
 }
 
 // ユーザーIDのチェック
-if (!isset($_SESSION['user']['id'])) {
+$user_id = $_SESSION['user']['id'] ?? null;
+if (!$user_id) {
     header('Location: create.php');
     exit;
 }
@@ -19,14 +20,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $title = $_POST['title'] ?? '';
     $contents = $_POST['contents'] ?? '';
 
+    // タイトルと内容のどちらかが空ならエラー
     if (empty($title) || empty($contents)) {
         $error_message = "タイトルか内容の入力がありません";
     } else {
-        $pdo = new PDO('mysql:host=mysql; dbname=blog; charset=utf8', 'root', 'password');
-        $stmt = $pdo->prepare("INSERT INTO blogs (title, contents, user_id) VALUES (?, ?, ?)");
-        $stmt->execute([$title, $contents, $user_id]);
-        header("Location: mypage.php");
-        exit;
+        // 保存処理
+        try {
+            $pdo = new PDO('mysql:host=mysql; dbname=blog; charset=utf8', 'root', 'password');
+            $stmt = $pdo->prepare("INSERT INTO blogs (title, contents, user_id) VALUES (?, ?, ?)");
+            $stmt->execute([$title, $contents, $user_id]);
+            header("Location: mypage.php");
+            exit;
+        } catch (Exception $e) {
+            $error_message = "保存に失敗しました。";
+        }
     }
 }
 
