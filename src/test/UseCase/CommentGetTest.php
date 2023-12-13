bb60df27028b9fb121ad364c24aa31ed943d5cfa
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-12-13 22:40:25 +0900

    feat:12月13日　CommentGetTest

---
+++ src/test/UseCase/CommentGetTest.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use App\UseCase\UseCaseInteractor\CommentGetInteractor;
+use App\Adapter\QueryServise\CommentQueryService;
+use App\UseCase\UseCaseInput\CommentInput;
+use App\Domain\ValueObject\Index\BlogId;
+use App\Infrastructure\Dao\CommentDao;
+
+final class CommentGetTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function コメントが正常に取得できる()
+    {
+        $input = new CommentInput(
+            new BlogId(1)
+        );
+
+        $commentDao = new CommentDao();
+        $commentQueryService = new CommentQueryService($commentDao);
+
+        $interactor = new CommentGetInteractor($input, $commentQueryService);
+
+        $comments = $interactor->getCommentsByBlogId();
+
+        $this->assertIsArray($comments, 'コメントの取得に失敗しました');
+    }
+}
+?>
