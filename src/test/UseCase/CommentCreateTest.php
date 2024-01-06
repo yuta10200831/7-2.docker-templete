82d57ee9faf18d11716f35f7b5d7cfd4a6f3de8b
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-01-06 21:33:41 +0900

    feat:test-CommentCreate

--- src/test/UseCase/CommentCreateTest.php
+++ src/test/UseCase/CommentCreateTest.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 use App\UseCase\UseCaseInteractor\CommentCreateInteractor;
 use App\UseCase\UseCaseInput\CommentInput;
-use App\Domain\Port\IComment;
 use App\Domain\Port\ICommentQuery;
 use App\Domain\ValueObject\Index\BlogId;
 use App\Domain\ValueObject\Index\CommentText;
@@ -18,23 +17,16 @@ final class CommentCreateTest extends TestCase
     {
         $input = new CommentInput(new BlogId(1), new CommentText('Unique comment'));
 
-        $commentInterface = new class implements IComment {
+        $commentQueryServiceMock = new class implements ICommentQuery {
+            public function findByBlogId(BlogId $blogId): array {
+                return [];
+            }
             public function storeComment(BlogId $blogId, string $comment, string $commenterName, int $userId): bool {
                 return true;
             }
-
-            public function findByBlogId(BlogId $blogId): array {
-                return [];
-            }
         };
 
-        $commentQueryInterface = new class implements ICommentQuery {
-            public function findByBlogId(BlogId $blogId): array {
-                return [];
-            }
-        };
-
-        $interactor = new CommentCreateInteractor($input, $commentInterface, $commentQueryInterface);
+        $interactor = new CommentCreateInteractor($input, $commentQueryServiceMock);
 
         $_SESSION['user']['name'] = 'testUser';
         $_SESSION['user']['id'] = 123;
@@ -47,60 +39,20 @@ final class CommentCreateTest extends TestCase
     /**
      * @test
      */
-    public function 同じコメントが既に存在する()
-    {
-        $input = new CommentInput(new BlogId(1), new CommentText('Existing comment'));
-
-        $commentInterface = new class implements IComment {
-            public function storeComment(BlogId $blogId, string $comment, string $commenterName, int $userId): bool {
-                return true;
-            }
-
-            public function findByBlogId(BlogId $blogId): array {
-                return ['Existing comment'];
-            }
-        };
-
-        $commentQueryInterface = new class implements ICommentQuery {
-            public function findByBlogId(BlogId $blogId): array {
-                return ['Existing comment'];
-            }
-        };
-
-        $interactor = new CommentCreateInteractor($input, $commentInterface, $commentQueryInterface);
-
-        $_SESSION['user']['name'] = 'testUser';
-        $_SESSION['user']['id'] = 123;
-
-        $output = $interactor->handler();
-
-        $this->assertFalse($output->isSuccess(), 'コメントがすでに存在します');
-    }
-
-    /**
-     * @test
-     */
     public function コメントの保存に失敗する()
     {
         $input = new CommentInput(new BlogId(1), new CommentText(''));
 
-        $commentInterface = new class implements IComment {
+        $commentQueryServiceMock = new class implements ICommentQuery {
+            public function findByBlogId(BlogId $blogId): array {
+                return [];
+            }
             public function storeComment(BlogId $blogId, string $comment, string $commenterName, int $userId): bool {
                 return false;
             }
-
-            public function findByBlogId(BlogId $blogId): array {
-                return [];
-            }
         };
 
-        $commentQueryInterface = new class implements ICommentQuery {
-            public function findByBlogId(BlogId $blogId): array {
-                return [];
-            }
-        };
-
-        $interactor = new CommentCreateInteractor($input, $commentInterface, $commentQueryInterface);
+        $interactor = new CommentCreateInteractor($input, $commentQueryServiceMock);
 
         $_SESSION['user']['name'] = 'testUser';
         $_SESSION['user']['id'] = 123;
@@ -110,4 +62,3 @@ final class CommentCreateTest extends TestCase
         $this->assertFalse($output->isSuccess(), 'コメントの保存が失敗しました');
     }
 }
-?>
